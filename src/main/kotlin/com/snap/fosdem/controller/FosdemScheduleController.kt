89d07c344c7efd855d1bb853d80dc2e6b5912208
Roberto García Romero <92rober@gmail.com>
2024-01-24 23:51:24 +0100

    feat: privacy policy added

--- src/main/kotlin/com/snap/fosdem/controller/FosdemScheduleController.kt
+++ src/main/kotlin/com/snap/fosdem/controller/FosdemScheduleController.kt
@@ -4,17 +4,22 @@ import com.snap.fosdem.model.Stand
 import com.snap.fosdem.model.Track
 import com.snap.fosdem.model.Version
 import com.snap.fosdem.service.FosdemScraper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ResourceLoader
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.time.Instant
-import java.util.Date
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/")
 class FosdemScheduleController(
         private val scraper: FosdemScraper
 ) {
+    @Autowired
+    lateinit var resourceLoader: ResourceLoader
 
     @GetMapping("tracks/")
     fun getTracks(): List<Track> {
@@ -32,5 +37,13 @@ class FosdemScheduleController(
             version = "0.0.1",
             date = Date.from(Instant.now()).toString()
         )
+    }
+
+    @GetMapping("policy/")
+    fun getPrivacyPolicy(): String {
+        val resource = resourceLoader.getResource("classpath:policy.html")
+        val file: File = resource.file
+
+        return file.readText()
     }
 }
